28a96836d3852fc0351f58f6446a20ddb99738ad
HemantKumar4 <blackforest210604@gmail.com>
2024-11-08 18:49:59 +0530

    some minor changes

--- App/app/src/main/java/com/example/adminconnect/DashboardScreen.kt
+++ App/app/src/main/java/com/example/adminconnect/DashboardScreen.kt
@@ -1,12 +1,7 @@
 package com.example.adminconnect
 
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -21,29 +16,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -51,26 +33,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalMapOf
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import kotlinx.coroutines.launch
-import com.example.adminconnect.OngoingTab
-import com.example.adminconnect.HistoryTab
-import com.example.adminconnect.CreateTab
-
 
 
 @Composable
@@ -110,39 +81,6 @@ fun DashboardScreen(modifier: Modifier = Modifier) {
 }
 
 
-//@OptIn(ExperimentalPagerApi::class)    // here we will have a tab row to display current screen but not able to navigate through bottom incons
-//@Composable
-//fun SwipeNavigationApp() {
-//    val pagerState = rememberPagerState(pageCount = { 3 })
-//    val coroutineScope = rememberCoroutineScope()
-//
-//    Column(modifier = Modifier.fillMaxSize()) {
-//        // Tab Row to Display the Screen Titles
-//        TabRow(selectedTabIndex = pagerState.currentPage) {
-//            listOf("Ongoing", "History", "Create").forEachIndexed { index, title ->
-//                Tab(
-//                    text = { Text(text = title) },
-//                    selected = pagerState.currentPage == index,
-//                    onClick = {
-//                        coroutineScope.launch {
-//                            pagerState.animateScrollToPage(index)
-//                        }
-//                    }
-//                )
-//            }
-//        }
-//
-//        // Horizontal Pager to Swipe Between Screens
-//        HorizontalPager(state = pagerState, modifier = Modifier.weight(1f)) { page ->
-//            when (page) {
-//                0 -> OngoingTab()
-//                1 -> HistoryTab()
-//                2 -> CreateTab()
-//            }
-//        }
-//    }
-//}
-
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun SwipeNavigationApp(pagerState: PagerState) {
@@ -157,9 +95,10 @@ fun SwipeNavigationApp(pagerState: PagerState) {
 
 @Composable
 fun BottomAppBarIcon(
-    icon: ImageVector,
+    icon: Painter,
     onClick: () -> Unit,
-    label: String
+    label: String,
+    iconSize: Dp
 ) {
     IconButton(onClick = onClick,
         modifier = Modifier
@@ -170,7 +109,7 @@ fun BottomAppBarIcon(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Icon(icon, contentDescription = null)
+            Icon(icon, contentDescription = null,modifier = Modifier.size(iconSize))
             Text(text = label)
         }
     }
@@ -218,25 +157,28 @@ fun DashboardBottomBar(pagerState: PagerState) {
                 modifier = Modifier.fillMaxWidth()
             ) {
                 BottomAppBarIcon(
-                    icon = Icons.Filled.Send,
+                    icon = painterResource(id = R.drawable.ongoing_icon),
                     onClick = {
                         coroutineScope.launch { pagerState.animateScrollToPage(0) }
                     },
-                    label = "Ongoing"
+                    label = "Ongoing",
+                    iconSize = 27.dp
                 )
                 BottomAppBarIcon(
-                    icon = Icons.Filled.Refresh,
+                    icon = painterResource(id = R.drawable.history_icon),
                     onClick = {
                         coroutineScope.launch { pagerState.animateScrollToPage(1) }
                     },
-                    label = "History"
+                    label = "History",
+                    iconSize = 27.dp
                 )
                 BottomAppBarIcon(
-                    icon = Icons.Filled.Add,
+                    icon = painterResource(id = R.drawable.create_icon),
                     onClick = {
                         coroutineScope.launch { pagerState.animateScrollToPage(2) }
                     },
-                    label = "Create"
+                    label = "Create",
+                    iconSize = 27.dp // Adjust size here
                 )
             }
         }
@@ -272,7 +214,7 @@ fun DashboardBottomBar(pagerState: PagerState) {
 @Composable
 fun Profile_info() {
 
-    val expanded = remember { mutableStateOf(true) }
+    val expanded = remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
