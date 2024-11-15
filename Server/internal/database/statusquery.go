8ad9c569022ead52ce45faf47087ce43fb06d039
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2024-11-15 11:36:33 +0530

    History API created

--- Server/internal/database/statusquery.go
+++ Server/internal/database/statusquery.go
@@ -21,3 +21,18 @@ func GetOngoingQuery(db *sql.DB, userId int) model.Queries {
 	}
 	return output
 }
+func GetHistoryQuery(db *sql.DB, userId int) model.Queries {
+	var output model.Queries
+	rows, err := db.Query("SELECT registration_no, created_at, current_status FROM status WHERE userid=$1 AND current_status = $2", userId, "Completed")
+	if err != nil {
+		return output
+	}
+	defer rows.Close()
+	for rows.Next(){
+		var query model.Query
+		if rows.Scan(&query.RegistrationId, &query.Date, &query.Status) == nil {
+			output.Data = append(output.Data, query)
+		}
+	}
+	return output
+}
